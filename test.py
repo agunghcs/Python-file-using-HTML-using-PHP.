7c4d3ebce93106826c8c2a93fc5b311117317e12
mamasagung <65334552+mamasagung@users.noreply.github.com>
2023-12-06 06:57:16 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,35 @@
-a = 2000
-b = 21
-print(f"a = {a}")
-print(f"b = {b}")
-print(f"a + b = {a + b}")
+import requests
+import xmltodict
+import json
+import time
+
+def getWarningBMGK():
+    response = requests.get(
+        "https://bmkg-content-inatews.storage.googleapis.com/warninggeof.xml"
+    )
+    return response.text
+
+
+def parseXML(xml):
+    return xmltodict.parse(xml)
+
+
+def main():
+    xml = getWarningBMGK()
+    doc = parseXML(xml)
+    data = f"""
+[!] {doc['alert']['info']['event']} :
+
+> Magnitudo : {doc['alert']['info']['magnitude']}
+> Kedalaman : {doc['alert']['info']['depth']}
+> Lokasi Gempa : {doc['alert']['info']['area']}
+> Arahan : {doc['alert']['info']['potential']}
+> Saran BMKG : {doc['alert']['info']['instruction']}
+
+"{doc['alert']['info']['description']}"
+    """
+    print(data)
+
+
+if __name__ == "__main__":
+    main()
